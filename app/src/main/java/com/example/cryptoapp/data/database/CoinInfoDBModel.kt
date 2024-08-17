c3a542216e6914fb936295d761b986c91023b8bb
SolomonVP <Lum7023@yandex.ru>
2024-08-17 22:17:20 +0300

    Работа с data-слоем

---
+++ app/src/main/java/com/example/cryptoapp/data/database/CoinInfoDBModel.kt
@@ -0,0 +1,35 @@
+package com.example.cryptoapp.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "full_price_list")
+data class CoinInfoDBModel(
+    @PrimaryKey
+//    @SerializedName("FROMSYMBOL")
+//    @Expose
+    val fromSymbol: String,
+//    @SerializedName("TOSYMBOL")
+//    @Expose
+    val toSymbol: String?,
+//    @SerializedName("PRICE")
+//    @Expose
+    val price: String?,
+//    @SerializedName("LASTUPDATE")
+//    @Expose
+    val lastUpdate: Long?,
+//    @SerializedName("HIGHDAY")
+//    @Expose
+    val highDay: String?,
+//    @SerializedName("LOWDAY")
+//    @Expose
+    val lowDay: String?,
+//    @SerializedName("LASTMARKET")
+//    @Expose
+    val lastMarket: String?,
+//    @SerializedName("IMAGEURL")
+//    @Expose
+    val imageUrl: String?
+)
