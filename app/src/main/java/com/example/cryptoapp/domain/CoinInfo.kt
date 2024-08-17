0efa089d79fbd538c4f65265aa14d754e4e61869
SolomonVP <Lum7023@yandex.ru>
2024-08-17 20:13:12 +0300

    Создаем Domain-слой

---
+++ app/src/main/java/com/example/cryptoapp/domain/CoinInfo.kt
@@ -0,0 +1,33 @@
+package com.example.cryptoapp.domain
+
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class CoinInfo(
+    @PrimaryKey
+    @SerializedName("FROMSYMBOL")
+    @Expose
+    val fromSymbol: String,
+    @SerializedName("TOSYMBOL")
+    @Expose
+    val toSymbol: String?,
+    @SerializedName("PRICE")
+    @Expose
+    val price: String?,
+    @SerializedName("LASTUPDATE")
+    @Expose
+    val lastUpdate: Long?,
+    @SerializedName("HIGHDAY")
+    @Expose
+    val highDay: String?,
+    @SerializedName("LOWDAY")
+    @Expose
+    val lowDay: String?,
+    @SerializedName("LASTMARKET")
+    @Expose
+    val lastMarket: String?,
+    @SerializedName("IMAGEURL")
+    @Expose
+    val imageUrl: String?
+)
